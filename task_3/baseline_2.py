fb126962a8070459070af573f52ede9b9c2fa8f8
Tutorial Account - tutorial044 <tutorial044@login01.athena.cyfronet.pl>
2025-03-15 22:40:06 +0100

    make better results saving into csv

--- task_3/baseline_2.py
+++ task_3/baseline_2.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch.utils.data import DataLoader
 from torchvision.models import resnet50
@@ -9,16 +8,17 @@ from train_utils import *
 import datetime
 import yaml
 
+
 def main():
     with open("config.yaml", "r") as file:
         config = yaml.safe_load(file)
 
     DEVICE = "cpu"
 
-    model = resnet50(weights='IMAGENET1K_V1')
+    model = resnet50(weights="IMAGENET1K_V1")
     model.fc = torch.nn.Linear(model.fc.weight.shape[1], 10)
 
-    data = torch.load('data.pt', weights_only=False)
+    data = torch.load("data.pt", weights_only=False)
     data.transform = transforms.Compose(
         [
             transforms.Resize((32, 32)),
@@ -28,16 +28,19 @@ def main():
     )
 
     # train_data, test_data = torch.utils.data.random_split(data, [99000, 1000])
-    train_data, test_data = torch.utils.data.random_split(data, [99900, 100])
-    train_loader = DataLoader(train_data, batch_size=config["batch_size"], shuffle=True, num_workers=16)
-    test_loader = DataLoader(test_data, batch_size=config["batch_size"], shuffle=True, num_workers=16)
+    train_data, test_data = torch.utils.data.random_split(data, [99990, 10])
+    train_loader = DataLoader(
+        train_data, batch_size=config["batch_size"], shuffle=True, num_workers=16
+    )
+    test_loader = DataLoader(
+        test_data, batch_size=config["batch_size"], shuffle=True, num_workers=16
+    )
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
     criterion = torch.nn.CrossEntropyLoss()
 
-
     model.to(DEVICE)
-    
+
     num_epochs = config["num_epochs"]
     save_dir = config["save_dir"]
     use_early_stopping = config["use_early_stopping"]
@@ -47,21 +50,24 @@ def main():
     save_dir = os.path.join(save_dir, str(datetime.datetime.now()))
     os.mkdir(save_dir)
 
-    lines = [f'Optimizer: {type(optimizer).__name__}',
-             f'Device: {DEVICE}',
-             f'epochs: {num_epochs}',
-             f'Learning rate: {optimizer.state_dict()["param_groups"][0]["lr"]}',
-             f'L2 penalty: {optimizer.state_dict()["param_groups"][0]["weight_decay"]}',
-             f'Batch size: {config["batch_size"]}',
-             f'use_early_stopping: {use_early_stopping}',
-             f'patience: {patience}',
-             f'min_delta: {min_delta}']
+    lines = [
+        f"Optimizer: {type(optimizer).__name__}",
+        f"Device: {DEVICE}",
+        f"epochs: {num_epochs}",
+        f'Learning rate: {optimizer.state_dict()["param_groups"][0]["lr"]}',
+        f'L2 penalty: {optimizer.state_dict()["param_groups"][0]["weight_decay"]}',
+        f'Batch size: {config["batch_size"]}',
+        f"use_early_stopping: {use_early_stopping}",
+        f"patience: {patience}",
+        f"min_delta: {min_delta}",
+    ]
 
-    with open(os.path.join(save_dir, 'hyperparams.txt'), 'w') as f:
-        f.write('\n'.join(lines))
+    with open(os.path.join(save_dir, "hyperparams.txt"), "w") as f:
+        f.write("\n".join(lines))
 
     # Training model
-    train(model=model,
+    train(
+        model=model,
         train_dataloader=test_loader,
         optimizer=optimizer,
         loss_fn=criterion,
@@ -73,7 +79,9 @@ def main():
         verbose=True,
         use_early_stopping=use_early_stopping,
         patience=patience,
-        min_delta=min_delta)
+        min_delta=min_delta,
+    )
+
 
 if __name__ == "__main__":
     main()
--- task_3/train_utils.py
+++ task_3/train_utils.py
@@ -8,9 +8,11 @@ from torchvision.transforms import v2
 
 from adversary import *
 
-'''
+"""
 source: https://github.com/jeffheaton/app_deep_learning/blob/main/t81_558_class_03_4_early_stop.ipynb
-'''
+"""
+
+
 class EarlyStopping:
     def __init__(self, patience=5, min_delta=0):
         self.patience = patience
@@ -39,15 +41,19 @@ class EarlyStopping:
         return False
 
 
-'''
+"""
 source: https://www.learnpytorch.io/04_pytorch_custom_datasets/#75-create-train-test-loop-functions
-'''
-def train_step(model: nn.Module, 
-               dataloader: torch.utils.data.DataLoader, 
-               loss_fn: nn.Module, 
-               optimizer: torch.optim.Optimizer,
-               device: str):
-    
+"""
+
+
+def train_step(
+    model: nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    device: str,
+):
+
     model.train()
     train_loss, train_total = 0, 0
     train_score, train_score_fgsm, train_score_pgd = 0, 0, 0
@@ -61,18 +67,18 @@ def train_step(model: nn.Module,
 
         y_pred = model(X)
         clean_loss = loss_fn(y_pred, y)
-        
+
         fgsm_pred = model(fgsm)
         fgsm_loss = loss_fn(fgsm_pred, y)
 
         pgd_pred = model(pgd)
         pgd_loss = loss_fn(pgd_pred, y)
 
-        loss = 0.5*clean_loss + 0.25*fgsm_loss + 0.25*pgd_loss
+        loss = 0.5 * clean_loss + 0.25 * fgsm_loss + 0.25 * pgd_loss
         train_loss += loss.item()
-        clean_loss_total += clean_loss
-        fgsm_loss_total += fgsm_loss
-        pgd_loss_total += pgd_loss
+        clean_loss_total += clean_loss.item()
+        fgsm_loss_total += fgsm_loss.item()
+        pgd_loss_total += pgd_loss.item()
 
         loss.backward()
         optimizer.step()
@@ -85,18 +91,30 @@ def train_step(model: nn.Module,
     train_loss = train_loss
     train_score = train_score / train_total
 
-    return train_loss, train_score, train_score_fgsm, train_score_pgd, clean_loss_total, fgsm_loss_total, pgd_loss_total
+    return (
+        train_loss,
+        train_score,
+        train_score_fgsm,
+        train_score_pgd,
+        clean_loss_total,
+        fgsm_loss_total,
+        pgd_loss_total,
+    )
 
 
-'''
+"""
 source: https://www.learnpytorch.io/04_pytorch_custom_datasets/#75-create-train-test-loop-functions
-'''
-def test_step(model: nn.Module, 
-              dataloader: torch.utils.data.DataLoader, 
-              loss_fn: nn.Module,
-              device: str):
-    
-    model.eval() 
+"""
+
+
+def test_step(
+    model: nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: nn.Module,
+    device: str,
+):
+
+    model.eval()
     test_loss, test_total = 0, 0
     test_score, test_score_fgsm, test_score_pgd = 0, 0, 0
     clean_loss_total, fgsm_loss_total, pgd_loss_total = 0, 0, 0
@@ -115,11 +133,11 @@ def test_step(model: nn.Module,
         pgd_pred = model(pgd)
         pgd_loss = loss_fn(pgd_pred, y)
 
-        loss = 0.5*clean_loss + 0.25*fgsm_loss + 0.25*pgd_loss
+        loss = 0.5 * clean_loss + 0.25 * fgsm_loss + 0.25 * pgd_loss
         test_loss += loss.item()
-        clean_loss_total += clean_loss
-        fgsm_loss_total += fgsm_loss
-        pgd_loss_total += pgd_loss
+        clean_loss_total += clean_loss.item()
+        fgsm_loss_total += fgsm_loss.item()
+        pgd_loss_total += pgd_loss.item()
 
         test_score += (torch.argmax(y_pred, dim=1) == y).sum().item()
         test_score_fgsm += (torch.argmax(fgsm_pred, dim=1) == y).sum().item()
@@ -128,97 +146,155 @@ def test_step(model: nn.Module,
 
     test_loss = test_loss
     test_score = test_score / test_total
-    
-    return test_loss, test_score, test_score_fgsm, test_score_pgd, clean_loss_total, fgsm_loss_total, pgd_loss_total
 
+    return (
+        test_loss,
+        test_score,
+        test_score_fgsm,
+        test_score_pgd,
+        clean_loss_total,
+        fgsm_loss_total,
+        pgd_loss_total,
+    )
 
 
 def save_model(model, save_path, file_name):
     torch.save(obj=model.state_dict(), f=os.path.join(save_path, file_name))
 
+
 def save_training_results(results, save_path, file_name):
-    results.to_csv(os.path.join(save_path, file_name), index=False, sep=',')
+    results.to_csv(os.path.join(save_path, file_name), index=False, sep=",")
 
 
-def train(model: nn.Module, 
-          train_dataloader: torch.utils.data.DataLoader,  
-          optimizer: torch.optim.Optimizer,
-          loss_fn: nn.Module,
-          device: str,
-          test_dataloader: torch.utils.data.DataLoader = None,
-          epochs: int = 20,
-          save_n_epochs: int = 5,
-          save_dir: str = './training_logs',
-          verbose: bool = True,
-          use_early_stopping: bool = True,
-          patience: int = 5,
-          min_delta: int = 0):
-    
+def train(
+    model: nn.Module,
+    train_dataloader: torch.utils.data.DataLoader,
+    optimizer: torch.optim.Optimizer,
+    loss_fn: nn.Module,
+    device: str,
+    test_dataloader: torch.utils.data.DataLoader = None,
+    epochs: int = 20,
+    save_n_epochs: int = 5,
+    save_dir: str = "./training_logs",
+    verbose: bool = True,
+    use_early_stopping: bool = True,
+    patience: int = 5,
+    min_delta: int = 0,
+):
+
     results = {
         "epoch": [],
         "train_loss": [],
         "train_score": [],
-        "val_loss": [],
-        "val_score": []
-        }
+        "train_score_fgsm": [],
+        "train_score_pgd": [],
+        "train_clean_loss": [],
+        "train_fgsm_loss": [],
+        "train_pgd_loss": [],
+        "test_loss": [],
+        "test_score": [],
+        "test_score_fgsm": [],
+        "test_score_pgd": [],
+        "test_clean_loss": [],
+        "test_fgsm_loss": [],
+        "test_pgd_loss": [],
+    }
     results = pd.DataFrame(results)
 
     # Creating directories for saving results and models
-    results_dir_path = os.path.join(save_dir, 'results')
-    models_dir_path = os.path.join(save_dir, 'models')
+    results_dir_path = os.path.join(save_dir, "results")
+    models_dir_path = os.path.join(save_dir, "models")
     os.mkdir(results_dir_path)
     os.mkdir(models_dir_path)
-    
+
     # Set early stopping object
     early_stop = EarlyStopping(patience=patience, min_delta=min_delta)
     early_stopping_used = False
-    use_early_stopping = ((test_dataloader is not None) and use_early_stopping)
+    use_early_stopping = (test_dataloader is not None) and use_early_stopping
 
     progress = tqdm(range(epochs))
 
     for epoch in progress:
-        train_loss, train_score, train_score_fgsm, train_score_pgd, train_clean_loss, train_fgsm_loss, train_pgd_loss = train_step(model=model,
-                                           dataloader=train_dataloader,
-                                           loss_fn=loss_fn,
-                                           optimizer=optimizer,
-                                           device=device)
+        (
+            train_loss,
+            train_score,
+            train_score_fgsm,
+            train_score_pgd,
+            train_clean_loss,
+            train_fgsm_loss,
+            train_pgd_loss,
+        ) = train_step(
+            model=model,
+            dataloader=train_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            device=device,
+        )
         if not test_dataloader == None:
-            test_loss, test_score, test_score_fgsm, test_score_pgd, test_clean_loss, test_fgsm_loss, test_pgd_loss = test_step(model=model,
-                                            dataloader=test_dataloader,
-                                            loss_fn=loss_fn,
-                                            device=device)
+            (
+                test_loss,
+                test_score,
+                test_score_fgsm,
+                test_score_pgd,
+                test_clean_loss,
+                test_fgsm_loss,
+                test_pgd_loss,
+            ) = test_step(
+                model=model, dataloader=test_dataloader, loss_fn=loss_fn, device=device
+            )
         else:
             test_loss = None
             test_score = None
-        
+
         if verbose:
             if not test_dataloader == None:
                 progress.set_description(
                     f"Epoch: {epoch+1} | "
                     f"test_score: {test_score:.4f} | "
                     f"test_score_fgsm: {test_score_fgsm:.4f} | "
-                    f"test_score_pgd: {test_score_pgd:.4f}")
+                    f"test_score_pgd: {test_score_pgd:.4f}"
+                )
             else:
                 progress.set_description(
-                    f"Epoch: {epoch+1} | "
-                    f"train_score: {train_score:.4f}")
-        
-        results.loc[len(results.index)] = [int(epoch+1), train_loss, train_score, test_loss, test_score]
+                    f"Epoch: {epoch+1} | " f"train_score: {train_score:.4f}"
+                )
+
+        results.loc[len(results.index)] = [
+            int(epoch + 1),
+            train_loss,
+            train_score,
+            train_score_fgsm,
+            train_score_pgd,
+            train_clean_loss,
+            train_fgsm_loss,
+            train_pgd_loss,
+            test_loss,
+            test_score,
+            test_score_fgsm,
+            test_score_pgd,
+            test_clean_loss,
+            test_fgsm_loss,
+            test_pgd_loss,
+        ]
 
         # Saving training results
-        save_training_results(results, results_dir_path, f'training_results_epoch_{epoch+1}.csv')
-        if (epoch+1) % save_n_epochs == 0 and not(epoch+1 == epochs):
-            save_model(model, models_dir_path, f'model_epoch_{epoch+1}')
+        save_training_results(
+            results, results_dir_path, f"training_results_epoch_{epoch+1}.csv"
+        )
+        if (epoch + 1) % save_n_epochs == 0 and not (epoch + 1 == epochs):
+            save_model(model, models_dir_path, f"model_epoch_{epoch+1}")
 
         if use_early_stopping and early_stop(model, test_loss):
             early_stopping_used = True
-            save_training_results(results, results_dir_path, f'training_results_early_stopping.csv')
-            save_model(model, models_dir_path, 'model_early_stopping')
+            save_training_results(
+                results, results_dir_path, f"training_results_early_stopping.csv"
+            )
+            save_model(model, models_dir_path, "model_early_stopping")
             print("Early stopping!")
             break
-    
+
     if not early_stopping_used:
-        save_training_results(results, results_dir_path, 'training_results_final.csv')
-        save_model(model, models_dir_path, 'model_final')
+        save_training_results(results, results_dir_path, "training_results_final.csv")
+        save_model(model, models_dir_path, "model_final")
         model.load_state_dict(early_stop.best_model)
-        save_model(model, models_dir_path, 'model_best')
+        save_model(model, models_dir_path, "model_best")
